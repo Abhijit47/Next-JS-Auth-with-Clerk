b009daeaf7cfcd346e2bc1abcd462b1ebc2a2692
Abhijit <akarmakar846@gmail.com>
2024-02-19 13:56:39 +0530

    Add afterAuth middleware to handle auth manually.

--- middleware.js
+++ middleware.js
@@ -1,4 +1,10 @@
-import { authMiddleware } from '@clerk/nextjs';
+import {
+  authMiddleware,
+  redirectToSignIn,
+  redirectToSignUp,
+} from '@clerk/nextjs';
+
+import { NextResponse } from 'next/server';
 
 export default authMiddleware({
   // Routes that can be access while signed out
@@ -7,16 +13,35 @@ export default authMiddleware({
     '/about',
     '/contact-us',
     '/next.svg',
-    '/sign-in',
-    '/sign-up',
+    // '/sign-in',
+    // '/sign-up',
   ],
 
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: ['dashboard'],
+  // ignoredRoutes: ['/dashboard'],
+  // Routes that require authentication
+  // privateRoutes: ['/dashboard'],
   // enable extra level debugging
   // debug: true,
-  // after sign out user should be redirect to home page
+
+  afterAuth(auth, req, evt) {
+    // console.log(auth, evt);
+    // console.log(req.url); // http://localhost:3000/
+
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    // If the user is logged in and trying to access a protected route, allow them to access route
+    if (auth.userId && !auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+
+    // Allow users visiting public routes to access them
+    return NextResponse.next();
+  },
 });
 
 export const config = {
